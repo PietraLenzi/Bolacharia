e1058a4ca45eb8a4d5cd4d1741c0c53b1730261d
PIETRA CARDOSO LENZI <pietra_lenzi@estudante.sesisenai.org.br>
2024-01-16 14:49:29 -0300

    criamos colecctions e fizemos a sincronia com a firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('serviceAccountKey.json');
+const serviceAccount = require('C:\\Users\\pietra_lenzi\\Desktop\\BolachariaApi\\serviceAccountKey.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
